111de3239e5d6dd7d33434bea12e4c457326a406
Jean-Baptiste RENAUDIN <jean-baptiste.renaudin@orange.com>
2014-10-22 16:49:01 +0200

    Ajout des premiers tests avec hibernate

--- src/fr/chezwams/budget/domain/repository/PersonRepository.java
+++ src/fr/chezwams/budget/domain/repository/PersonRepository.java
@@ -1,14 +1,31 @@
 package fr.chezwams.budget.domain.repository;
 
-import static java.util.Arrays.asList;
-
 import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
 
 import fr.chezwams.budget.domain.Person;
 
 public class PersonRepository {
 
 	public List<Person> all() {
-		return asList(new Person("JB"), new Person("Angy"));
+		Configuration configuration = new Configuration()
+			.setProperty("hibernate.connection.driver_class", "com.mysql.jdbc.Driver")
+			.setProperty("hibernate.connection.url", "jdbc:mysql://localhost/budget")
+			.setProperty("hibernate.connection.username", "root")
+			.setProperty("hibernate.connection.password", "test")
+			.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect")
+			.setProperty("hibernate.show_sql", "true")
+			.setProperty("hibernate.hbm2ddl.auto", "update")
+			.addFile("src/fr/chezwams/budget/domain/repository/hbm/Person.hbm.xml");
+		StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties());
+		SessionFactory factory = configuration.buildSessionFactory(builder.build());
+		Session session = factory.openSession();
+		Query query = session.createQuery("from Person");
+        return query.list();
 	}
 }
